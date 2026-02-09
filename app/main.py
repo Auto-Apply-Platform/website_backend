cbf2de3cf48bbf44fbad27942925d2933cfdc0ca
Сергей Жиров <s.zhirov@iqdev.digital>
2026-02-09 05:11:54 +0000

    Update file main.py

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ async def lifespan(_: FastAPI):
 app = FastAPI(title=settings.app_name, lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://reqbot.iqdev.team"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
